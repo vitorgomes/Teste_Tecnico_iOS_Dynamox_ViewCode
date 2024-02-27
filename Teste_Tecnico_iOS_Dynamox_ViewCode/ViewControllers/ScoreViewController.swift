f8d426c68abb028faafca9bb9ec46cd5280eda53
Vitor Gomes <vitorgomes_silva@outlook.com>
2024-02-27 12:05:25 -0300

    Update ScoreViewController: Programmatically add a UITableView (scoreTableView) to display players' scores, set up constraints in setupViews(), and add extensions for DataSource and Delegate methods (numberOfRowsInSection and cellForRowAt). Also add a new UILabel (tableTitleLabel) between restartButton and the table to act as the title of the table. Remove the for loop and print statement from viewDidLoad used to print the entire persistent data from CoreData to the console logs

--- Teste_Tecnico_iOS_Dynamox_ViewCode/ViewControllers/ScoreViewController.swift
+++ Teste_Tecnico_iOS_Dynamox_ViewCode/ViewControllers/ScoreViewController.swift
@@ -29,6 +29,25 @@ class ScoreViewController: UIViewController {
         return restartButton
     }()
     
+    private let tableTitleLabel: UILabel = {
+        let tableTitleLabel = UILabel()
+        
+        tableTitleLabel.text = "Tabela de Scores"
+        tableTitleLabel.font = UIFont.boldSystemFont(ofSize: 18)
+        tableTitleLabel.textAlignment = .center
+        tableTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableTitleLabel
+    }()
+    
+    private let scoreTableView: UITableView = {
+        let scoreTableView = UITableView()
+        
+        scoreTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return scoreTableView
+    }()
+    
     init(viewModel: ScoreViewModel) {
         self.scoreViewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -43,12 +62,6 @@ class ScoreViewController: UIViewController {
         
         setupViews()
         
-        let players = scoreViewModel.fetchPlayers() ?? []
-        print("*** PLAYERS SCORES TABLE ***")
-        for player in players {
-            print("Username: \(player.userName), Score: \(player.score)")
-        }
-        
         navigationItem.hidesBackButton = true
     }
     
@@ -59,23 +72,55 @@ class ScoreViewController: UIViewController {
         
         view.addSubview(scoreLabel)
         view.addSubview(restartButton)
+        view.addSubview(tableTitleLabel)
+        view.addSubview(scoreTableView)
         
         let screenSize = UIScreen.main.bounds
         let screenWidth = screenSize.width
         let restartButtonWidth = screenWidth * 0.8
         
         NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
             scoreLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            scoreLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
             
             restartButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             restartButton.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor, constant: 16),
             restartButton.heightAnchor.constraint(equalToConstant: 48),
-            restartButton.widthAnchor.constraint(equalToConstant: restartButtonWidth)
+            restartButton.widthAnchor.constraint(equalToConstant: restartButtonWidth),
+            
+            tableTitleLabel.topAnchor.constraint(equalTo: restartButton.bottomAnchor, constant: 16),
+            tableTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            scoreTableView.topAnchor.constraint(equalTo: tableTitleLabel.bottomAnchor, constant: 16),
+            scoreTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scoreTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scoreTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+        
+        scoreTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        scoreTableView.dataSource = self
+        scoreTableView.delegate = self
     }
     
     @objc private func restartButtonTapped() {
         navigationController?.popToRootViewController(animated: true)
+    }
+}
+
+extension ScoreViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return scoreViewModel.fetchPlayers()?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        if let players = scoreViewModel.fetchPlayers() {
+            let player = players[indexPath.row]
+            cell.textLabel?.text = "Username: \(player.userName ?? ""), Score: \(player.score)"
+        }
+        
+        return cell
     }
 }
